859efa3e40603284f32869e4d7c6f3b2e6b4bcd3
Sumeet Mourya <go69ciq@mytum.de>
2024-06-25 01:14:46 +0200

    Wrapping the apps around SessionProvider context in merchant-app.

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -2,6 +2,8 @@ import { getServerSession } from "next-auth"
 import { NextResponse } from "next/server";
 import { authOptions } from "../../lib/auth";
 
+//getServerSession(authOptions) --> lets you check if a user is logged in on the server side before sending back a response
+//GET calls getServerSession with authOptions to get the current user's session.
 export const GET = async () => {
     const session = await getServerSession(authOptions);
     if (session.user) {
