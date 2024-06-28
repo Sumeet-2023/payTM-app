42f659400f22f967e88bde64d5ecbf3d1a6d9cf1
Sumeet Mourya <go69ciq@mytum.de>
2024-06-29 00:45:03 +0200

    added ui component and changes to  transfer page.

--- apps/user-app/app/(dashboard)/transfer/page.tsx
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -1,5 +1,12 @@
+
+import  { AddMoney } from "../../../components/AddMoneyCard"
 export default function() {
     return <div>
-        Transfer Page
+         <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
+            Transfer
+        </div>
+            <div>
+                <AddMoney />
+            </div>
     </div>
 }
