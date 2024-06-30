4981bce6bdf80d538c249fa02b174ebed9241556
Sumeet Mourya <go69ciq@mytum.de>
2024-07-01 00:01:11 +0200

    updated page on transfer route.

--- apps/user-app/app/(dashboard)/transfer/page.tsx
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -1,12 +1,59 @@
 
 import  { AddMoney } from "../../../components/AddMoneyCard"
-export default function() {
-    return <div>
-         <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
-            Transfer
-        </div>
-            <div>
-                <AddMoney />
-            </div>
-    </div>
+import { getServerSession } from "next-auth"
+import { BalanceCard } from "../../../components/BalanceCard"
+import { OnRampTransactions } from "../../../components/OnRampTransaction"
+import prisma from "@repo/db/client"
+import { authOptions } from "../../lib/auth"
+
+async function getBalance() {
+    const session = await getServerSession(authOptions)
+    const balance = await prisma.balance.findFirst({
+        where : {
+            userId : Number(session?.user?.id)
+        }
+    });
+    return {
+        amount : balance?.amount || 0,
+        locked : balance?.locked || 0
+    }
+    
+}
+async function onRamp() {
+    const session = await getServerSession(authOptions)
+    const trnx = await prisma.onRampTransaction.findMany({
+        where : {
+            userId : Number(session?.user.id)
+        }
+    });
+    return trnx.map(t => ({
+    time: t.startTime,
+    amount: t.amount,
+    status: t.status,
+    provider: t.provider
+    
+    }))
+}
+
+export default async function() {
+
+   const balance = await getBalance();
+   const transactions = await onRamp();
+
+   return <div className="w-screen">
+   <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
+       Transfer
+   </div>
+   <div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-4">
+       <div>
+           <AddMoney />
+       </div>
+       <div>
+           <BalanceCard amount={balance.amount} locked={balance.locked} />
+           <div className="pt-4">
+               <OnRampTransactions transactions={transactions} />
+           </div>
+       </div>
+   </div>
+</div>
 }
