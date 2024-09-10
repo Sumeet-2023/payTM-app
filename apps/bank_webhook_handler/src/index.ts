1c7b91138158e70a235f2234d3d36e120bd6f957
Sumeet Mourya <go69ciq@mytum.de>
2024-09-10 15:37:15 +0200

    Changes in bank_webhook backend.

--- apps/bank_webhook_handler/src/index.ts
+++ apps/bank_webhook_handler/src/index.ts
@@ -3,6 +3,8 @@ import db from "@repo/db/client";
 
 const app = express();
 
+app.use(express.json())
+
 app.post("/hdfcWebhook", async (req, res) => {
     // Add zod validation 
     const paymentInformation: {
